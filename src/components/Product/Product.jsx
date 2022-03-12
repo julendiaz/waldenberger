7a29c2ccf95bc6dc73e1d35a8a8e149b4a996131
julendiaz <iamjulendiaz@gmail.com>
2022-03-12 12:37:48 +0100

    Add styles for Product Card

--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -1,20 +1,47 @@
 import { FavoriteBorderOutlined, SearchOutlined, ShoppingCartOutlined } from "@material-ui/icons"
 import { Container, Image, Info, Icon } from "./Product.style"
+import styled from "styled-components"; 
 
+const ProductDetailsContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    /* background-color: blue; */
+    margin-left: 1.8em;
+`
+const ProductName = styled.h3 `
+`
+const ProductPrice = styled.h3`
+    font-weight: 300;
+`
+
+const ButtonAddToCard = styled.button`
+    background-color: black;
+    width: 9em;
+    height: 3.3em;
+    font-size: 0.8em;
+    font-weight: 400;
+    color: white;
+    /* border-radius: 2em; */
+    padding: 0.8em;
+    border: none;
+    cursor: pointer;
+    margin-right: 1.8em;
+    transition: all 0.2s ease;
+
+    &:hover {
+        background-color: #292929;
+    }
+`
 const Product = ({item}) => {
   return (
     <Container>
         <Image src={item.img} />
         <Info>
-            <Icon>
-                <ShoppingCartOutlined />
-            </Icon>
-            <Icon>
-                <SearchOutlined />
-            </Icon>
-            <Icon>
-                <FavoriteBorderOutlined />
-            </Icon>
+            <ProductDetailsContainer>
+                <ProductName>White t-shirt</ProductName>
+                <ProductPrice>23.00€</ProductPrice>
+            </ProductDetailsContainer>
+            <ButtonAddToCard>Add to Cart</ButtonAddToCard>
         </Info>
     </Container>
   )
--- src/components/Product/Product.style.jsx
+++ src/components/Product/Product.style.jsx
@@ -1,17 +1,25 @@
 import styled from "styled-components"
 
 export const Info = styled.div`
-    opacity: 0;
+    opacity: 1;
     width: 100%;
-    height: 100%;
+    height: 5em;
     position: absolute;
-    top: 0;
+    bottom: 0;
     left: 0;
-    background-color: rgba(0,0,0, 0.1);
+    background-color: rgb(255,255,255, 0.8);
     z-index: 3;
     display: flex;
-    align-items: center;
-    justify-content: center;
+    align-items: flex-end;
+    justify-content: space-between;
+    transition: all 0.5s ease;
+    padding-bottom: 1.3em;
+`
+
+export const Image = styled.img`
+    height: 75%;
+    z-index: 2;
+    opacity: 0.7;
     transition: all 0.5s ease;
 `
 
@@ -25,16 +33,20 @@ export const Container = styled.div`
     justify-content: center;
     background-color: white;
     position: relative;
+    cursor: pointer;
 
     &:hover ${Info}{
         opacity: 1;
+        background-color: white;
+    }
+
+    &:hover ${Image}{
+        opacity: 1;
+        transform: scale(1.03);
     }
 `
 
-export const Image = styled.img`
-    height: 75%;
-    z-index: 2;
-`
+
 
 
 
